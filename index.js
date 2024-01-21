442a1c1251b90b3dde4cb7b271e16e8605e0b18a
Tik7578 <157278631+Tik7578@users.noreply.github.com>
2024-01-21 10:27:55 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = ["🇮🇹Tokcraft🇮🇹", Tok7875"];
 
 
 let currentIndex = 0;
